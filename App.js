d4f215e16b6234936839fc2b526aabd8ce5c3046
arifsatriya <98566695+arifsatriya@users.noreply.github.com>
2024-11-19 13:02:13 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
+import Table from "./components/Table";
+
 const App = () => {
- return <>App</>;
+ return (
+  <div className="table_container">
+   <h1>Sortable table with React</h1>
+   <Table />
+  </div>
+ );
 };
 
 export default App;
